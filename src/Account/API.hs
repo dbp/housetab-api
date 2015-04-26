5442c90d63688fe59d0dc9d7b399b5ab2055bfb7
Daniel Patterson <dbp@dbpmail.net>
2015-04-26 18:29:03 -0400

    add basic session functionality - creating them, validating them, touching them

--- src/Account/API.hs
+++ src/Account/API.hs
@@ -1,29 +1,39 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE OverloadedStrings, RecordWildCards, DeriveGeneric #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PolyKinds         #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Account.API where
 
-import Control.Monad (liftM)
-import Data.Text (Text)
-import qualified Data.Text.Encoding as TE
-import qualified Data.ByteString as B
-import Control.Monad.IO.Class (liftIO)
-import Opaleye (runQuery, runInsertReturning, pgStrictText, pgStrictByteString)
-import Data.Profunctor.Product.Default (def)
-import GHC.Int (Int64)
-import GHC.Generics
-import System.Random (randomRIO)
-import Data.Aeson (ToJSON)
+import           Control.Monad                   (liftM)
+import           Control.Monad.IO.Class          (MonadIO, liftIO)
+import           Control.Monad.Trans.Either
+import           Data.Aeson                      (ToJSON)
+import           Data.ByteString                 (ByteString)
+import qualified Data.ByteString                 as B
+import           Data.ByteString.Internal        (c2w)
+import           Data.Profunctor.Product.Default (def)
+import           Data.Text                       (Text)
+import qualified Data.Text                       as T
+import qualified Data.Text.Encoding              as TE
+import           GHC.Generics
+import           GHC.Int                         (Int64)
+import           Opaleye                         (pgStrictByteString,
+                                                  pgStrictText,
+                                                  runInsertReturning, runQuery)
+import           System.Random                   (randomRIO)
 
-import qualified Crypto.Hash.SHA512 as SHA512
+import qualified Crypto.Hash.SHA512              as SHA512
 
-import Database.PostgreSQL.Simple
+import qualified Database.PostgreSQL.Simple      as PG
+import qualified Database.Redis                  as R
 
-import Servant
-import Account.Types
+import           Account.Types
+import           Servant
 
 
 data Authentication = Authed Text
@@ -32,47 +42,85 @@ data Authentication = Authed Text
 instance ToJSON Authentication
 
 type AccountApi = "accounts" :> ReqBody NewAccount :> Post Account
-             :<|> "accounts" :> "authenticate"
+             :<|> "accounts" :> "session" :> "new"
                              :> Capture "name" Text
                              :> QueryParam "password" Text
                              :> Get Authentication
+             :<|> "accounts" :> "session" :> "check"
+                             :> QueryParam "token" Text
+                             :> Get Bool
+             :<|> "accounts" :> "session" :> "touch"
+                             :> QueryParam "token" Text
+                             :> Get Bool
              :<|> "accounts" :> Capture "name" Text :> Get Account
              :<|> "accounts" :> Get [Account]
 
-server :: Connection -> Server AccountApi
-server conn = postAccount
-         :<|> authenticate
-         :<|> getAccount
-         :<|> getAccounts
-  where genSalt :: IO B.ByteString
-        genSalt = do chars <- sequence (take 64 (repeat (randomRIO (0,64))))
-                     return $ B.pack chars
-        hashPassword :: Text -> B.ByteString -> B.ByteString
-        hashPassword cleartext salt =
-          let a = B.append salt (TE.encodeUtf8 cleartext)
-          in (iterate SHA512.hash a) !! 512
-        conv :: NewAccount -> IO NewAccountColumn
-        conv (Account {..}) = do
-          salt <- genSalt
-          let hash = hashPassword accountPassword salt
-          return $ Account { accountId = Nothing
-                           , accountName = pgStrictText accountName
-                           , accountEmail = pgStrictText accountEmail
-                           , accountPassword = pgStrictByteString hash
-                           , accountSalt = pgStrictByteString salt
-                           }
-        one :: Text -> IO Account
-        one name = liftM head $ runQuery conn (getAccountQuery name)
-        postAccount account =
-          do account' <- liftIO $ conv account
-             [account''] <- liftIO $ runInsertReturning conn accountTable account' id
-             return account''
-        authenticate name (Just password) =
-          do account <- liftIO $ one name
-             let authenticated =
-                   accountPassword account == hashPassword password (accountSalt account)
-             return $ if authenticated
-                         then Authed "111"
-                         else NotAuthed
-        getAccount name     = liftIO $ one name
-        getAccounts         = liftIO $ runQuery conn accountQuery
+sessionSeconds :: Integer
+sessionSeconds = 60*60*24
+
+genSalt :: IO ByteString
+genSalt = do chars <- sequence (take 64 (repeat (randomRIO ('a','z'))))
+             return $ B.pack (map c2w chars)
+hashPassword :: Text -> ByteString -> ByteString
+hashPassword cleartext salt =
+  let a = B.append salt (TE.encodeUtf8 cleartext)
+  in (iterate SHA512.hash a) !! 512
+sessionKey :: Text -> ByteString
+sessionKey token = B.append "housetab:session:" (TE.encodeUtf8 token)
+
+generateSessionToken :: R.Connection -> Int -> IO Text
+generateSessionToken r accountId =
+  do token <- genSalt
+     R.runRedis r (R.setex (sessionKey (TE.decodeUtf8 token)) sessionSeconds (TE.encodeUtf8 (T.pack (show accountId))))
+     return (TE.decodeUtf8 token)
+
+touchSession r (Just token) =
+   do r <- liftIO $ R.runRedis r (R.expire (sessionKey token) sessionSeconds)
+      case r of
+        Left _ -> return False
+        Right _ -> return True
+
+checkSession r (Just token) =
+  do ex <- liftIO $ R.runRedis r (R.exists (sessionKey token))
+     case ex of
+       Right True -> return True
+       _ -> return False
+
+conv :: NewAccount -> IO NewAccountColumn
+conv (Account {..}) = do
+  salt <- genSalt
+  let hash = hashPassword accountPassword salt
+  return $ Account { accountId = Nothing
+                   , accountName = pgStrictText accountName
+                   , accountEmail = pgStrictText accountEmail
+                   , accountPassword = pgStrictByteString hash
+                   , accountSalt = pgStrictByteString salt
+                   }
+one :: PG.Connection -> Text -> IO Account
+one pg name = liftM head $ runQuery pg (getAccountQuery name)
+
+postAccount pg account =
+  do account' <- liftIO $ conv account
+     [account''] <- liftIO $ runInsertReturning pg accountTable account' id
+     return account''
+
+authenticate pg r name (Just password) =
+  do account <- liftIO $ one pg name
+     let authenticated =
+           accountPassword account == hashPassword password (accountSalt account)
+     if authenticated
+        then do token <- liftIO (generateSessionToken r (accountId account))
+                return $ Authed token
+        else return NotAuthed
+
+getAccount pg name = liftIO $ one pg name
+
+getAccounts pg = liftIO $ runQuery pg accountQuery
+
+server :: PG.Connection -> R.Connection -> Server AccountApi
+server pg r = postAccount pg
+             :<|> authenticate pg r
+             :<|> checkSession r
+             :<|> touchSession r
+             :<|> getAccount pg
+             :<|> getAccounts pg
--- src/Main.hs
+++ src/Main.hs
@@ -9,15 +9,17 @@ import Database.PostgreSQL.Simple (connectPostgreSQL)
 import Servant (serve)
 import Network.Wai.Handler.Warp (run)
 import Data.Proxy (Proxy(..))
+import Database.Redis (connect, defaultConnectInfo)
 
 accountApi :: Proxy AccountApi
 accountApi = Proxy
 
 main :: IO ()
 main = do
-  conn <- connectPostgreSQL "host=localhost \
-                            \user=housetab_user \
-                            \dbname=housetab_devel \
-                            \password=111"
+  pg <- connectPostgreSQL "host=localhost \
+                          \user=housetab_user \
+                          \dbname=housetab_devel \
+                          \password=111"
+  redis <- connect defaultConnectInfo
   putStrLn "Listening on port 8000..."
-  run 8000 (serve accountApi $ server conn)
+  run 8000 (serve accountApi $ server pg redis)
